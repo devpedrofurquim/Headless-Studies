421f8c7569209c4d0b87f0ff217c8cd910e05d18
devpedrofurquim <pedrofurquim.dev@gmail.com>
2024-08-20 17:44:46 -0300

    Maybe fixed

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
 import { name as appName } from './app.json';
 import MyHeadlessTask from './src/HeartBeat';
+import MainApp from './MainApp';
 
 const App = () => (
-  <HeartBeatProvider>
-    <MainApp />
-  </HeartBeatProvider>
+    <MainApp/>
 );
 
-AppRegistry.registerHeadlessTask('Heartbeat', () => MyHeadlessTask);
 AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerHeadlessTask('Heartbeat', () => MyHeadlessTask);
--- src/HeartBeat.js
+++ src/HeartBeat.js
@@ -1,5 +1,11 @@
+import { DeviceEventEmitter } from 'react-native';
 import { NativeModules } from 'react-native';
-
 const { Heartbeat } = NativeModules;
+
+const MyHeadlessTask = async (event) => {
+    console.log('Heartbeat event received:', event);
+};
+
+DeviceEventEmitter.addListener('HeartbeatEvent', Heartbeat);
 
 export default Heartbeat;
